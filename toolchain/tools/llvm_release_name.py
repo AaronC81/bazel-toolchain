7ebbe8b04d07638471f6054fbb3641c23e1d8fff
Aaron Christiansen <aaronc20000@gmail.com>
2019-10-29 23:34:48 +0000

    Add Manjaro support

--- toolchain/tools/llvm_release_name.py
+++ toolchain/tools/llvm_release_name.py
@@ -64,7 +64,7 @@ def _linux(llvm_version):
         os_name = "linux-gnu-ubuntu-14.04"
     elif distname == "ubuntu" and version.startswith("18.04"):
         os_name = "linux-gnu-ubuntu-18.04"
-    elif distname in ["arch", "ubuntu"] or (distname == "linuxmint" and version.startswith("18")):
+    elif distname in ["arch", "ubuntu", "manjaro"] or (distname == "linuxmint" and version.startswith("18")):
         os_name = "linux-gnu-ubuntu-16.04"
     elif distname == "debian" and int(version) >= 8:
         os_name = "linux-gnu-debian8"
